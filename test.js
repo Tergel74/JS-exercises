b80f4340d209d5c3031da0f887878cce64fc999e
Tergel <tergel7006@gmail.com>
2022-06-24 12:49:13 +0800

    made user folder for the file that stores the users and the js file for functionality and some test files for testing

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-function sendCode() {
-    code = Math.round(Math.random(1000, 9999))
-    return '    The code for verifying is: ' + code + '.'
+var fs = require('fs')
+
+var player = {
+    userName: 'Tegi',
+    password: '123',
+    id: 2,
 }
 
-console.log(sendCode.code)
+fs.readFile('test.json', function (err, data) {
+    if (err) console.log(err);
+    var players = JSON.parse(data);
+    players.push(player);
+    var add_players = JSON.stringify(players, null, 2);
+    fs.writeFileSync('test.json', add_players)
+})
