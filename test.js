abdc10a53a9980bd8390720af8bcd394d78b5422
Tergel <tergel7006@gmail.com>
2022-07-05 10:05:28 +0800

    added gitignore file to ignore changes in node modules folder and worked on test file to test smt

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-var fs = require('fs')
 
-var player = {
-    userName: 'Tegi',
-    password: '123',
-    id: 2,
+if ('Tergel' == 'Tergel') {
+    console.log(true)
 }
-
-fs.readFile('test.json', function (err, data) {
-    if (err) console.log(err);
-    var players = JSON.parse(data);
-    players.push(player);
-    var add_players = JSON.stringify(players, null, 2);
-    fs.writeFileSync('test.json', add_players)
-})
